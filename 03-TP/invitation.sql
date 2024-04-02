23b11b51aa29b82964d938020fedd1714abdbf65
Lait Entier <axelzinc@hotmail.fr>
2024-04-02 12:28:34 +0200

    fix nom des variables

--- 03-TP/invitation.sql
+++ 03-TP/invitation.sql
@@ -34,8 +34,8 @@ CREATE TABLE inv_personne (
     nom VARCHAR(255) NOT NULL,
     age INT NOT NULL,
     inscription DATE NOT NULL,
-    status BOOLEAN NOT NULL,
-    type ENUM('membre', 'non membre') NOT NULL,
-    description TEXT NOT NULL,
+    status_ins BOOLEAN NOT NULL,
+    type_ ENUM('membre', 'non membre') NOT NULL,
+    description_ins TEXT NOT NULL,
     salaire_annuel DECIMAL(10, 2) NOT NULL
 ) ENGINE=InnoDB;
