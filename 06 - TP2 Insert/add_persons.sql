2aa7dafdc9a8549ba8891544bdc12353a1f90954
Lait Entier <axelzinc@hotmail.fr>
2024-04-02 16:38:10 +0200

    Remove unnecessary comments

--- 06 - TP2 Insert/add_persons.sql
+++ 06 - TP2 Insert/add_persons.sql
@@ -1,17 +1,3 @@
-/* 
-CREATE TABLE inv_personne (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    prenom VARCHAR(255) NOT NULL,
-    nom VARCHAR(255) NOT NULL,
-    age INT NOT NULL,
-    inscription DATE NOT NULL,
-    status_validation BOOLEAN NOT NULL,
-    type_ ENUM('membre', 'non membre') NOT NULL,
-    description_ins TEXT NOT NULL,
-    salaire_annuel DECIMAL(10, 0) NOT NULL
-) ENGINE=InnoDB;
-*/
-
 -- clear table
 DELETE FROM inv_personne;
 
@@ -20,28 +6,6 @@ INSERT INTO inv_personne (prenom,nom,age,inscription,status_validation,type_,des
 ('Brad', 'PITT', 60, '1970-01-01', TRUE, 'non membre', 'aussi vieux que jean castex', 2000000),
 ('George', 'CLOONEY', 62, '1999-01-01', TRUE, 'membre', 'juste beau', 4000000),
 ('Jean','DUJARDIN', 51, '1994-01-01', FALSE, 'membre', 'brice de nice', 1000000);
-
-/*
-    afficher le plus gros chiffre d'affaire (avec MAX)
-
-    afficher le plus petit chiffre d'affaire (avec MIN)
-
-    afficher le nom de la personne du plus petit CA
-
-    afficher le nom de la personne du plus gros CA
-
-    afficher le CA moyen
-
-    afficher le nombre de client
-
-    proposer une requete avec BETWEEN
-
-    proposer une requete avec UCASE(), UPPER(),LCASE(),LOWER()
-
-    proposer une requete avec LIKE '%..%'
-
-    Trier par age les membres
-*/
 
 -- afficher le plus gros salaire (avec MAX)
 SELECT MAX(salaire_annuel) FROM inv_personne;
